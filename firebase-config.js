83d3f030d0b65cc1973e189fe69e8ad53150ffe4
Fadil <muhammadfadilstemsend@gmail.com>
2026-08-20 11:03:11 +0700

    ffd

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC9--urH7kmLPjOuNtjC6WKa9Wx6XjcG4s",
+  authDomain: "zientax-b93cc.firebaseapp.com",
+  projectId: "zientax-b93cc",
+  storageBucket: "zientax-b93cc.firebasestorage.app",
+  messagingSenderId: "55660481725",
+  appId: "1:55660481725:web:4ff5aae3a80d14cab27bd4"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
